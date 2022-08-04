7065c4348e7690d49a4fad428716efcee6e2d6da
JFYAzong <1018720708@qq.com>
2022-08-04 11:21:12 +0800

    Update 解法1.js

--- 1.两数之和/解法1.js
+++ 1.两数之和/解法1.js
@@ -3,14 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     let map = new Map();
-    for(let i = 0, len = nums.length; i < len; i++){
-        if(map.has(target - nums[i])){
+    for (let i = 0, len = nums.length; i < len; i++) {
+        if (map.has(target - nums[i])) {
             return [map.get(target - nums[i]), i];
-        }else{
+        } else {
             map.set(nums[i], i);
         }
     }
     return [];
 };
+
+a = twoSum([1,2,3,4], 5);
+console.log(a);
