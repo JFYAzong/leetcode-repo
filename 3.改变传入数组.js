781770f1511e38032685ca88dc2bfdd70a3c0742
JFYAzong <1018720708@qq.com>
2022-08-17 14:55:30 +0800

    1

--- 3.改变传入数组.js
+++
@@ -1,6 +0,0 @@
-var arr = [1,2,3,4];
-function putFitst(arr, n){
-    if(Array.isArray(arr)){
-        
-    }
-}
--- 改变传入的数组，将数组中第 n(从 0 开始算 ) 个元素放到数组的开头.js
+++ 改变传入的数组，将数组中第 n(从 0 开始算 ) 个元素放到数组的开头.js
@@ -2,5 +2,8 @@ var arr = [1, 2, 3, 4];
 function putFirst(arr, n) {
     if (Array.isArray(arr)) {
         var item = arr.splice(n, 1)[0];
+        arr.unshift(item);
     }
 }
+var a = putFirst(arr,2);
+console.log(a);
