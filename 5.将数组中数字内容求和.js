98970d11574a84e821ccc69fac759d6535103b7a
JFYAzong <1018720708@qq.com>
2022-08-19 11:21:10 +0800

    Update 5.将数组中数字内容求和.js

--- 5.将数组中数字内容求和.js
+++ 5.将数组中数字内容求和.js
@@ -1,22 +1,14 @@
-function sum (arr) {
+function sum(arr) {
     var res = 0;
-    if(Array.isArray(arr)){
-        if(arr.reduce){
-            res = arr.reduce(function (prev, curr) {
-                if(typeof curr === 'number'){
-                    return prev + curr;
-                }
-                return prev;
-            }, 0)
-        } else {
-            arr.forEach(function  (each) {
-                if(typeof each === 'number'){
-                    res = res + each;
-                }
-            });
-        }
+    if (Array.isArray(arr)) {
+        res = arr.reduce(function (prev, curr) {
+            if (typeof curr === 'number') {
+                return prev + curr;
+            }
+            return prev;
+        }, 0)
     }
     return res;
 }
-var a = sum(['a', 1, 2, {a:3}, 3, 4, 1.2]);
+var a = sum([1, 2, 3, 4, 1.2]);
 console.log(a)
